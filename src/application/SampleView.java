3a27a428d5a0603718af77b202812d41a0a3df55
Christian Biasi <christianbiasi@gmx.de>
2019-06-01 02:56:17 +0200

    built lower left track-control UI

--- src/application/SampleView.java
+++ src/application/SampleView.java
@@ -1,10 +1,15 @@
 package application;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class SampleView extends StackPane {
 
@@ -26,11 +31,41 @@ public class SampleView extends StackPane {
 	public SampleView() {
 		initImageViews();
 		
+		Label leftLabel = new Label("L");
+		leftLabel.setStyle("-fx-font-size: 2em; -fx-text-fill: white");
+		Label rightLabel = new Label("R");
+		rightLabel.setStyle("-fx-font-size: 2em; -fx-text-fill: white");
+
+		VBox leftTrack = (VBox) buildVBox(linkerTrackVorImageView, linkerTrackZurueckImageView, 5);
+		VBox rightTrack = (VBox) buildVBox(rechterTrackVorImageView, rechterTrackZurueckImageView, 5);
+		VBox leftLabelAndTrack = (VBox) buildVBox(leftLabel, leftTrack, 10);
+		VBox rightLabelAndTrack = (VBox) buildVBox(rightLabel, rightTrack, 10);
+		HBox leftControls = (HBox) buildHBox(leftLabelAndTrack, rightLabelAndTrack, 5);
+		leftControls.setPadding(new Insets(10));
+
+		BorderPane bottomBar = new BorderPane();
+		bottomBar.setStyle("-fx-background-color: #000");
+		bottomBar.setLeft(leftControls);
+		bottomBar.setAlignment(leftControls, Pos.CENTER);
+		
 		BorderPane mainBorderPane = new BorderPane();
-		mainBorderPane.setCenter(linkerTrackVorImageView);
+		mainBorderPane.setBottom(bottomBar);
 		getChildren().add(mainBorderPane);
 	}
 	
+	private Node buildHBox(Node leftItem, Node rightItem, int spacing) {
+		HBox hbox = new HBox(leftItem, rightItem);
+		hbox.setSpacing(spacing);
+		return hbox;
+	}
+
+	private Node buildVBox(Node upperItem, Node lowerItem, int spacing) {
+		VBox vbox = new VBox(upperItem, lowerItem);
+		vbox.setAlignment(Pos.CENTER);
+		vbox.setSpacing(spacing);
+		return vbox;
+	}
+
 	private void initImageViews() {
 		linkerTrackVorImageView = initImageView("pictures/Bilder alt/linkerTrackVor.png", linkerTrackVorImageView);
 		linkerTrackZurueckImageView = initImageView("pictures/Bilder alt/linkerTrackZurueck.png", linkerTrackZurueckImageView);
@@ -49,6 +84,9 @@ public class SampleView extends StackPane {
 	private ImageView initImageView(String imagePath, ImageView imageView) {
 		image = new Image(getClass().getResourceAsStream(imagePath));
 		imageView = new ImageView(image);
+		imageView.setFitHeight(75);
+		imageView.setFitWidth(75);
+		imageView.setPreserveRatio(true);
 		return imageView;
 	}
 
