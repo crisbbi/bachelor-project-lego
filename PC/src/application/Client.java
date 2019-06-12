b7a701987d917a96190f17fff1cafbca5d03150e
Celine <celine.thermann@student.uni-luebeck.de>
2019-06-12 15:56:42 +0200

    better network search

--- PC/src/application/Client.java
+++ PC/src/application/Client.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
@@ -19,22 +20,48 @@ public class Client {
 	 * 
 	 * @param ip
 	 * @param port
+	 * @throws  
 	 */
-	public Client(String ip, int port) {
+	public Client(String ip, int port)   {
 
 		String ipnew = ip;
-		socket = new Socket();
-		for (int i = 0; i < 256; i++) {
-			try {
-				ipnew = ip;
-				ipnew += Integer.toString(i);
-				socket.connect(new InetSocketAddress(ipnew, port), 200);
-				System.out.println("Richtiger boi ist bei: "+ipnew);
-				break;
-			} catch (Exception e) {
-				System.out.println("falscher boi :c");
-				
-			}
+		
+	        ProcessBuilder builder = new ProcessBuilder(
+	                "cmd.exe", "/c", "arp -a");
+	            builder.redirectErrorStream(true);
+	            Process p = null;
+				try {
+					p = builder.start();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	            String line = null;
+	            while (true) {
+	                try {
+						line = r.readLine();
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	                if (line == null) { break; }
+	                String[] lines = line.split("        ");
+					if(lines.length > 1 && lines[1].equals("b8-27-eb-86-d3-08     dynamisch ")) {
+						ipnew = lines[0];
+						ipnew = ipnew.substring(2, ipnew.length());
+						System.out.println(ipnew);
+						 break;
+					}
+	             
+	            }
+	     socket = new Socket();
+		try {
+			socket.connect(new InetSocketAddress(ipnew, port), 1000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
@@ -42,14 +69,12 @@ public class Client {
 	/**
 	 * Sends a message to the Host (PI)
 	 * 
-	 * @param message
-	 *            the message to send
+	 * @param message the message to send
 	 */
 	void sendMessage(String message) {
 		PrintWriter printWriter;
 		try {
-			printWriter = new PrintWriter(new OutputStreamWriter(
-					socket.getOutputStream()));
+			printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 			printWriter.print(message);
 			printWriter.flush();
 		} catch (IOException e) {
