06b9f2a423d8df2a7e36a847caa7d2331fc00c6e
Christian Biasi <christianbiasi@gmx.de>
2019-06-29 21:24:56 +0200

    included all images in MainActivity

--- Android/app/src/main/java/com/example/baprojekt2019/MainActivity.java
+++ Android/app/src/main/java/com/example/baprojekt2019/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.baprojekt2019;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -14,6 +15,16 @@ public class MainActivity extends AppCompatActivity{
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
+        ImageView fliessbandLinks = (ImageView) findViewById(R.id.fliessbandLinks);
+        ImageView fliessbandRechts = (ImageView) findViewById(R.id.fliessbandRechts);
+        ImageView schaufel = (ImageView) findViewById(R.id.schaufel);
+        ImageView licht = (ImageView) findViewById(R.id.licht);
+        ImageView notAus = (ImageView) findViewById(R.id.notAus);
+        ImageView turmDrehenUhrzeiger = (ImageView) findViewById(R.id.armDrehenUhrzeiger);
+        ImageView turmDrehenGegeneUhrzeiger = (ImageView) findViewById(R.id.armDrehenGegenUhrzeiger);
+        ImageView armHeben = (ImageView) findViewById(R.id.armHeben);
+        ImageView armSenken = (ImageView) findViewById(R.id.armSenken);
     }
 
 }
